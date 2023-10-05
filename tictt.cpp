edde9dbfab29a0c72c4e6a2d3ebcb2cb59e1dee8
U-BSD\412140 <412140@SHS-26W2NW3.beaverton.k12.or.us>
2023-10-05 10:00:22 -0700

    first

---
+++ tictt.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+using namespace std;
+void resetBoard(&board);
+
+int main()
+{
+  char board[3][3];  
+}
+
+void resetBoard(&board)
+{
+  for(int i =0; i <3; i++) 
+    {
+      for(int j =0; j<3; j++)
+	{
+	  board[i][j] = '-';
+	}
+    }
+}
+
+void printBoard()
+{
+  int x,y;
+  for()
+    {
+      
+    }
+}
+
