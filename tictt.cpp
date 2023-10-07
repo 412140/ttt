49c9a1b460dee3e9604d801aede848858828df81
412140 <144371963+412140@users.noreply.github.com>
2023-10-06 21:03:00 -0700

    Update tictt.cpp
    from home because terminal isnt working

--- tictt.cpp
+++ tictt.cpp
@@ -1,32 +1,62 @@
 #include <iostream>
 #include <cstring>
 #include <cctype>
+/*
+Ghazi Abbas
+Period 4
+C++ Fall sem
+very basic tic tac toe
+
+sources:
+https://en.cppreference.com/w/cpp/language/functions
+https://www.geeksforgeeks.org/difference-between-const-int-const-int-const-and-int-const/#
++ Galbraiths videos opn ampersand and pointers and arrays and function prototyping
+
+*/
 
 using namespace std;
-void resetBoard(&board);
+
+const int boardSize = 3; //https://www.geeksforgeeks.org/difference-between-const-int-const-int-const-and-int-const/#
+
+void resetBoard(char board[][boardSize]);
+void printBoard(char board[][boardSize]);
 
 int main()
 {
-  char board[3][3];  
+    char board[boardSize][boardSize];
+    resetBoard(board);
+    printBoard(board);
+
+    return 0;
 }
 
-void resetBoard(&board)
+void resetBoard(char board[][boardSize])
 {
-  for(int i =0; i <3; i++) 
+    for (int i = 0; i < boardSize; i++)
     {
-      for(int j =0; j<3; j++)
-	{
-	  board[i][j] = '-';
-	}
+        for (int j = 0; j < boardSize; j++)
+        {
+            board[i][j] = '-';
+        }
     }
 }
 
-void printBoard()
+void printBoard(char board[][boardSize])
 {
-  int x,y;
-  for()
+    cout << "  a b c" << endl;
+
+    for (int i = 0; i < boardSize; i++)
     {
-      
+        cout << i + 1 << " ";
+        for (int j = 0; j < boardSize; j++)
+        {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
     }
 }
+
+
+
+
 
