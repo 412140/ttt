616a5e8ed6c380edabc13e56c228b5ce1065a296
412140 <144371963+412140@users.noreply.github.com>
2023-10-06 22:40:20 -0700

    Update tictt.cpp
    finally done

--- tictt.cpp
+++ tictt.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <cstring>
-#include <cctype>
+
+using namespace std;
 /*
 Ghazi Abbas
 Period 4
@@ -12,51 +12,119 @@ https://en.cppreference.com/w/cpp/language/functions
 https://www.geeksforgeeks.org/difference-between-const-int-const-int-const-and-int-const/#
 + Galbraiths videos opn ampersand and pointers and arrays and function prototyping
 
+
 */
+const int boardSize = 3; // convenience sake
+const char emptyCell = ' ';//doing this instead of the old one where i was manually typing it in, also this way works better with the real board design i made
+const char playerX = 'X';
+const char playerO = 'O';
+//https://www.geeksforgeeks.org/difference-between-const-int-const-int-const-and-int-const/
 
-using namespace std;
+void initializeBoard(char board[boardSize][boardSize])//simple double for loop to target every part of the board and make it empty 
+{//galbraiths video ^
+    for (int i = 0; i < boardSize; ++i) 
+    {
+        for (int j = 0; j < boardSize; ++j) 
+        {
+            board[i][j] = emptyCell;
+        }
+    }
+}
 
-const int boardSize = 3; //https://www.geeksforgeeks.org/difference-between-const-int-const-int-const-and-int-const/#
+// kind of translated this from a computer hardware 2 assignment from last year which was actually in python, liked this board better than abc 123
+void printBoard(const char board[boardSize][boardSize]) 
+{
+    for (int i = 0; i < boardSize; ++i) 
+    {
+        for (int j = 0; j < boardSize; ++j) 
+        {
+            cout << board[i][j];
+            if (j < boardSize - 1)
+                cout << "|";
+        }
+        cout << "\n";
+        if (i < boardSize - 1)
+            cout << "-----\n";
+    }
+}
+//https://www.youtube.com/watch?v=j5B9GwJO8HA YouTube video i found on bool functions
+bool isMoveLegal(const char board[boardSize][boardSize], int row, int col) 
+{
+    return (row >= 0 && row < boardSize && col >= 0 && col < boardSize && board[row][col] == emptyCell);
+}
+//kind of similar to the java video one
+bool checkWin(const char board[boardSize][boardSize], char currentPlayer) 
+{
+    for (int i = 0; i < boardSize; ++i) 
+    {
+        if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) 
+            return true;
+        if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) 
+            return true;
+    }
+    if (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) 
+        return true;
+    if (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer) 
+        return true;
+    return false;
+}
 
-void resetBoard(char board[][boardSize]);
-void printBoard(char board[][boardSize]);
+bool isBoardFull(const char board[boardSize][boardSize]) 
+{
+    for (int i = 0; i < boardSize; ++i) 
+    {
+        for (int j = 0; j < boardSize; ++j) 
+        {
+            if (board[i][j] == emptyCell)
+                return false;
+        }
+    }
+    return true;
+}
 
-int main()
+int main() 
 {
     char board[boardSize][boardSize];
-    resetBoard(board);
-    printBoard(board);
+    initializeBoard(board);
+    char currentPlayer = playerX;
 
-    return 0;
-}
+    int winsX = 0, winsO = 0, ties = 0;
 
-void resetBoard(char board[][boardSize])
-{
-    for (int i = 0; i < boardSize; i++)
+    while (true) 
     {
-        for (int j = 0; j < boardSize; j++)
+        printBoard(board);
+
+        int row, col;
+        cout << "Player " << currentPlayer << ", enter your move (row[0-" << boardSize - 1 << "] col[0-" << boardSize - 1 << "]): ";
+        cin >> row >> col;
+	// enter row enter col this was an attempt at conserving space.
+        if (!isMoveLegal(board, row, col)) 
         {
-            board[i][j] = '-';
+            cout << "Invalid move. Try again.\n";
+            continue;
         }
-    }
-}
 
-void printBoard(char board[][boardSize])
-{
-    cout << "  a b c" << endl;
+        board[row][col] = currentPlayer;
 
-    for (int i = 0; i < boardSize; i++)
-    {
-        cout << i + 1 << " ";
-        for (int j = 0; j < boardSize; j++)
+        if (checkWin(board, currentPlayer)) 
         {
-            cout << board[i][j] << " ";
+            printBoard(board);
+            cout << "Player " << currentPlayer << " wins!\n";
+            if (currentPlayer == playerX)
+                winsX++;
+            else
+                winsO++;
+            initializeBoard(board);
+        } 
+        else if (isBoardFull(board)) 
+        {
+            printBoard(board);
+            cout << "It's a tie!\n";
+            ties++;
+            initializeBoard(board);
         }
-        cout << endl;
+
+        currentPlayer = (currentPlayer == playerX) ? playerO : playerX;
     }
+
 }
-
-
-
-
-
